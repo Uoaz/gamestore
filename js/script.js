acbe3ac8f6c43d56f86cd1b0b07e48379730cbc2
IbrahimZaou <ibrahimzaou3@gmail.com>
2022-08-28 17:25:18 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 // SLIDER 
 
-// const img = document.querySelectorAll('.slider article');
-// const nbSlide = img.length;
+const img = document.querySelectorAll('.slider article');
+const nbSlide = img.length;
 
-// let count = 0;
+let count = 0;
 
-// setInterval(function nextSlide() {
-//     img[count].classList.remove('active');
+setInterval(function nextSlide() {
+    img[count].classList.remove('active');
 
-//     if(count< nbSlide - 1) {
-//         count++;
-//     } else {
-//         count = 0;
-//     }
+    if(count< nbSlide - 1) {
+        count++;
+    } else {
+        count = 0;
+    }
 
-//     img[count].classList.add('active');
+    img[count].classList.add('active');
     
-// }, 2000);
+}, 5000);
