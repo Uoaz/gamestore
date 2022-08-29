73d9b6c5b68fa76952e6714b0fda72cc24263623
IbraZaou <ibrahimzaou3@gmail.com>
2022-08-29 09:46:26 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -17,3 +17,22 @@ setInterval(function nextSlide() {
     img[count].classList.add('active');
     
 }, 5000);
+
+
+// Image change quand la souris passe dessus
+
+
+const imgChange = document.querySelectorAll('.change');
+const secondeImg = document.querySelectorAll('.none')
+
+
+imgChange.addEventListener('mouseover', function(){
+
+    imgChange.style.display = "none";
+    secondeImg.classList.remove('none');
+})
+
+secondeImg.addEventListener('mouseleave', function(){
+    imgChange.style.display='block';
+    secondeImg.classList.add('none');
+})
