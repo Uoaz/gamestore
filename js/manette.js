65029bdf9c51ec0f70d25cffa4d99181995ec3a6
IbrahimZaou <ibrahimzaou3@gmail.com>
2022-08-31 23:20:27 +0200

    commit

---
+++ js/manette.js
@@ -0,0 +1,17 @@
+
+// Hover for controller color
+
+
+const xbox = document.querySelector('.xbox-controller');
+
+
+function changeImage() {
+
+    xbox.addEventListener('click', function(){
+
+        xbox.src = "../img/manette/xbox/xboxwhite.png";
+    });
+
+};
+
+
